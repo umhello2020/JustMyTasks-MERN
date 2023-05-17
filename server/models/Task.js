5d0b1d86a5e919e363cb2fc13611de566f403091
Olivia Gilbert <umhello2020@gmail.com>
2023-05-16 20:17:18 -0500

    added typeDefs and resolvers to schema, also tweaked models, and created controllers folder

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema(
   {
+    taskId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     title: {
       type: String,
       required: true,

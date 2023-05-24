841b07fb4733963ddf865eba95127722f60b7e65
colearrington98@gmail.com <colearrington98@gmail.com>
2023-05-23 21:32:47 -0500

    Update api endpoints

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,47 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+  baseURL: 'http://localhost:3000/api',
 });
 
-export default api;
+const apiEndpoints = {
+    createUser: (userData) => {
+        return api.post('/users', userData)
+            .then(response => response.data)
+            .catch(error => {
+                console.error('Error:', error);
+                throw error;
+            });
+    },
+
+  getTasks: () => {
+    return api.get('/tasks')
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error:', error);
+        throw error;
+      });
+  },
+
+  createTask: (taskData) => {
+    return api.post('/tasks', taskData)
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error:', error);
+        throw error;
+      });
+  },
+
+    updateTask: (taskId, taskData) => {
+        return api.put(`/tasks/${taskId}`, taskData)
+            .then(response => response.data)
+            .catch(error => {
+                console.error('Error:', error);
+                throw error;
+            });
+    }
+
+
+};
+
+export default apiEndpoints;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = erq.body.token || req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -19,21 +16,43 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (err) {
+      console.log('Invalid token:', err.message);
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
-    // send to next endpoint
     next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  login: function (credentials) {
+    // Assuming you have a User model/schema
+    const { username, password } = credentials;
+
+    // Check if the username and password match a user in the database
+    // You'll need to implement your own logic for this
+    const user = User.findOne({ username });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Check if the password is valid
+    const isValidPassword = user.comparePassword(password);
+
+    if (!isValidPassword) {
+      throw new Error('Invalid password');
+    }
+
+    // Generate and return a token
+    const token = this.signToken({ username, email: user.email, _id: user._id });
+    return token;
   },
 };
